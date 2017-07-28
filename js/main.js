07c525ac093d9412809aba962a4560d749a77b2f
Edward R <edwardrz@users.noreply.github.com>
2017-07-28 11:24:34 -0500

    updated: code complete

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 console.log("main.js active");
 
-/***********  SCRIPT  ***************/
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
+  var coinPurse = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
   }
 
-*/
+//Make the object empty to hold new values.  
+var coinPurse = {};
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
-  
-  coinPurse.quarters = 0;
+
+function coinCounter (num) {
+
+  //Convert total amount to a whole number to make math easy.
+  num = num * 100;
+
+  //Find the number of quarters and assign to object.
+  coinPurse.quarters = parseInt(num / 25);
+  //Find the remaining amount, assign to current amount.
+  num = num % 25;
+  console.log("quarters", coinPurse.quarters);
+
+  coinPurse.dimes = parseInt(num / 10);
+  num = num % 10;
+  console.log("dimes", num);
+
+  coinPurse.nickels = parseInt(num / 5);
+  num = num % 5;
+  console.log("nickels", num);
+
+  coinPurse.pennies = num; 
+  console.log("pennies", coinPurse.pennies);
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+coins = coinCounter(2.23)
+console.log("coins", coins);
 
 
